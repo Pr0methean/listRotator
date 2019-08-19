d349d41bba1a03187d675c1f5a0edfef7f992816
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2019-08-18 21:25:52 -0700

    Use an STSRA to improve randomization

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,21 @@
+import io.github.pr0methean.betterrandom.prng.concurrent.SingleThreadSplittableRandomAdapter
 import java.nio.file.Paths
 
 fun main(args : Array<String>) {
+    val random = SingleThreadSplittableRandomAdapter()
     val folder = Paths.get(if (args.isEmpty()) {""} else {args[0]}).toAbsolutePath().toFile()
     println("Working in $folder")
     val listFiles = folder.listFiles {
             file -> file.isFile
-    } ?: error("Error: argument must be a directory")
+    } ?: error("Argument must be a directory")
     println("Found files: ${listFiles.joinToString { file -> file.toString() }}")
     val lists = mutableListOf<List<String>>()
     var totalEntries = 0
     for (file in listFiles) {
         val lines = file.readLines(Charsets.UTF_8)
             .map { line -> line.split("#")[0] }
-            .filter { line -> line.isNotBlank() }.shuffled()
+            .filter { line -> line.isNotBlank() }
+            .shuffled(random)
         if (lines.isNotEmpty()) {
             lists.add(lines)
         }
